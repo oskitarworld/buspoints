45281917978ed49a2e479050ec485e6801725ad7
Oscar Linares <oscarl.rubi@gmail.com>
2026-01-01 14:11:40 +0100

    test: replace widget_test with simple app build test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,13 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('app builds', (WidgetTester tester) async {
-    // Monta una app mínima para evitar dependencias externas en tests
     await tester.pumpWidget(
       const MaterialApp(
         home: Scaffold(body: Center(child: Text('app running'))),
       ),
     );
-
     expect(find.text('app running'), findsOneWidget);
   });
 }
